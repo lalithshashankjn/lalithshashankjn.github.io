08d6c53a7c1fb992eb2226cedd6431bfbac45497
lalithshashank <lalithshashank.jn@gmail.com>
2020-07-10 06:02:39 +0530

    Modified, No cursor

---
+++ resizeImage.js
@@ -0,0 +1,11 @@
+
+function bigImage() {
+    image.style.width = 100%;
+    image.style.height = 100%;
+}
+
+function smallImage() {
+    image.style.width = 30px;
+    image.style.height = 30px;
+
+}
