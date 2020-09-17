d6091d6f2f8e1bc7999c9d523c3c74600a2263cd
lalithshashank <lalithshashank.jn@gmail.com>
2020-09-17 17:35:40 +0530

    Update colors

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function show() {
-    document.body.style.background = "green" ;    
-}
 
-function openlayers() {
-
+function draw() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
 }
