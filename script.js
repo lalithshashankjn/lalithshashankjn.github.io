62f93afe0e0f007913b111645187633338f1a87a
lalithshashank <lalithshashank.jn@gmail.com>
2020-09-19 15:00:56 +0530

    earth

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+let welcomeSphere;
+let body;
+let debug = true;
 
-function draw() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke();
+function initializeSphere() {    
+    body = document.getElementsByTagName("body");   
+    welcomeSphere = document.getElementById("welcomesphere");
+    //welcomeSphere.style.animationPlayState = "paused";
+    welcomeSphere.style.animationPlayState = "running";
 }
+
+function pauseAnimation() {    
+    welcomeSphere.style.animationPlayState = "paused";
+}
+
+function resumeAnimation() {    
+    welcomeSphere.style.animationPlayState = "running";
+}
+
+function hideThis() {
+    $(".sphereContainer").fadeOut("fast");
+}
+
+function showThis() {
+    $(".sphereContainer").fadeIn("fast");
+}
+
