747e8c71e3bc6cd376f9abecc5d18631b32ce985
lalithshashank <lalithshashank.jn@gmail.com>
2020-09-19 17:09:19 +0530

    sphere resize

--- script.js
+++ script.js
@@ -7,17 +7,30 @@ function initializeSphere() {
     welcomeSphere = document.getElementById("welcomesphere");    
     welcomeSphere.style.animationPlayState = "running";
     $(".mainContent").fadeOut("fast");
+    resizeSphere();
 }
 
 function resizeSphere() {
-    let width = $("#welcomesphere").width();
-    let height = $("#welcomesphere").height();
-    if (width < height) {        
-        $("#welcomesphere").width(height);
+    let containerWidth = $(".sphereContainer").width();
+    let containerHeight = $(".sphereContainer").height();
+    let sphereWidth = $("#welcomesphere").width();
+    let sphereHeight = $("#welcomesphere").height();
+    if ((containerWidth < containerHeight)) {
+        $("#welcomesphere").height(containerWidth);
+        $("#welcomesphere").width( containerWidth);
+    }
+    else if ((containerWidth > containerHeight)) {        
+        $("#welcomesphere").width(containerHeight);
+        $("#welcomesphere").height(containerHeight);
     }
     else {
-        $("#welcomesphere").height(width);        
+        $("#welcomesphere").width(containerWidth);
+        $("#welcomesphere").height(containerHeight);
     }
+    console.log("containerWidth: ", containerWidth);
+    console.log("containerHeight: ", containerHeight);
+    console.log("sphereWidth: ", $("#welcomesphere").width());
+    console.log("sphereHeight: ", $("#welcomesphere").height());
 }
 
 function pauseAnimation() {    
