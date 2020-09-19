da34a006af92b77539d36b522ff03dee158d9a86
lalithshashank <lalithshashank.jn@gmail.com>
2020-09-19 20:36:15 +0530

    keyframes with jQuery

--- script.js
+++ script.js
@@ -2,35 +2,40 @@ let welcomeSphere;
 let body;
 let debug = true;
 
+
 function initializeSphere() {    
     body = document.getElementsByTagName("body");   
     welcomeSphere = document.getElementById("welcomesphere");    
     welcomeSphere.style.animationPlayState = "running";
-    $(".mainContent").fadeOut("fast");
+    $(".mainContent").fadeOut("fast");    
     resizeSphere();
 }
 
 function resizeSphere() {
     let containerWidth = $(".sphereContainer").width();
-    let containerHeight = $(".sphereContainer").height();
-    let sphereWidth = $("#welcomesphere").width();
-    let sphereHeight = $("#welcomesphere").height();
+    let containerHeight = $(".sphereContainer").height();   
+    let setVal = 0;
     if ((containerWidth < containerHeight)) {
         $("#welcomesphere").height(containerWidth);
-        $("#welcomesphere").width( containerWidth);
+        $("#welcomesphere").width(containerWidth);        
     }
     else if ((containerWidth > containerHeight)) {        
         $("#welcomesphere").width(containerHeight);
-        $("#welcomesphere").height(containerHeight);
+        $("#welcomesphere").height(containerHeight);        
     }
     else {
         $("#welcomesphere").width(containerWidth);
         $("#welcomesphere").height(containerHeight);
-    }
-    console.log("containerWidth: ", containerWidth);
-    console.log("containerHeight: ", containerHeight);
-    console.log("sphereWidth: ", $("#welcomesphere").width());
-    console.log("sphereHeight: ", $("#welcomesphere").height());
+    }    
+
+    toBGPosition = getBGpxPosition($("#welcomesphere").width())+ ' 0';
+    $.keyframe.define([{
+        name: 'scaled-rotate',
+        from: {'background-position': '0 0'},
+        to: { 'background-position': toBGPosition }
+    }]);
+    $('#welcomesphere').playKeyframe(
+        ' scaled-rotate 5s linear infinite');
 }
 
 function pauseAnimation() {    
@@ -50,3 +55,9 @@ function showThis() {
     $(".sphereContainer").fadeIn();
 }
 
+function getBGpxPosition(x){
+    //let y = 48970850 + (-43.40962 - 48970850) / (1 + (x / 30829810) ^ 0.9658882);
+    let y = (x *1690)/ 844;
+    console.log("width: ", x, " position: ", y);
+    return y.toString();
+}
